60d5a89c8a5c26a2f5a6eb2d6d4c1e8d9c42dd7c
Roku <mr558869193@gmail.com>
2022-07-26 23:58:35 +0900

    変更：攻撃当たり判定

--- Assets/Script/Player/PlayerAtack.cs
+++ Assets/Script/Player/PlayerAtack.cs
@@ -5,6 +5,14 @@ using UnityEngine.InputSystem;
 
 public class PlayerAtack : MonoBehaviour
 {
+    [SerializeField, Header("攻撃力")]
+    int _atk = 10;
+    [SerializeField, Header("攻撃座標")]
+    Vector3 _attackPosPos ;
+    [SerializeField, Header("攻撃座標")]
+    float _attackRange;
+    [SerializeField, Header("攻撃座標")]
+    LayerMask _enemyLayers;
     Animator _animator;
     bool _isAtack = false;
     public bool IsAtack // プロパティ
@@ -35,6 +43,15 @@ public class PlayerAtack : MonoBehaviour
     void NormalAttack()
     {
         _animator.SetTrigger("Attack1");
+        ////コライダー出現
+        //Collider[] hitEnemies = Physics.OverlapSphere(_attackPosPos, _attackRange, _enemyLayers);
+        //foreach (Collider enemy in hitEnemies)
+        //{
+        //    if (enemy.gameObject.tag == "Enemy")
+        //    {
+        //        CallDamage(enemy);
+        //    }
+        //}
 
     }
 
@@ -52,6 +69,33 @@ public class PlayerAtack : MonoBehaviour
         _isAtack = true;
     }
 
+    /// <summary>
+    /// 対象のダメージ関数呼び出し処理
+    /// </summary>
+    /// <param name="collider"></param>
+    public void CallDamage(Collider collider)
+    {
+        //↓に拠点のダメージ処理があるスクリプトを指定
+        if (collider.gameObject.GetComponent<Enemy>())
+        {
+            collider.gameObject.GetComponent<Enemy>().GetDamage(_atk, collider);
+        }
+    }
 
+    /// <summary>
+    /// 打ち上げ用の関数
+    /// </summary>
+    //void Launch() 
+    //{
+    //    _rb.DOMoveY(7f, 0.5f); //Y方向に0.5秒かけて上昇
+    //    Collider[] hitEnemies = Physics.OverlapSphere(AttackPoint.position, AttackRange); //コライダー出現
+    //    foreach (Collider enemy in hitEnemies)
+    //    {
+    //        if(enemy.GetComponent<Enemy>())
+    //        {
+    //            enemy.GetComponent<Enemy>().Launch(); //攻撃を受けた敵を打ち上げます。
+    //        }
+    //    }
+    //}
 
 }
